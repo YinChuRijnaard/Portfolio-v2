1e17a3777bfc90a6ba6c42dafa79256a814559cc
Yin Chu <ycrijnaard@gmail.com>
2022-04-06 17:36:12 +0300

    Changed margin for Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="my-6 mx-8 flex h-screen flex-col items-center justify-center space-y-8 px-6 text-center font-serif text-4xl dark:text-white md:mx-36 lg:mx-72">
-      {/* Find other way to dead-center div above (not using my-6) */}
+    <div className="mx-8 flex h-screen flex-col items-center justify-center space-y-8 px-6 text-center font-serif text-4xl dark:text-white md:mx-36 lg:mx-72">
+      {/* Find other way to dead-center div above */}
       <p className="opacity-75">Hi, I'm</p>
       <p className="text-7xl">Yin Chu</p>
       <p className="opacity-75">

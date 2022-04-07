f8e6d39be6473eaf555092622ac41fb43e0b6338
Yin Chu <ycrijnaard@gmail.com>
2022-04-07 10:44:08 +0300

    Added smooth scrolling and button transition

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -9,7 +9,7 @@ const Error = () => {
         <p className="animate__animated animate__rubberBand text-9xl">404</p>
         <p className="text-6xl">Uh oh...</p>
         <p>The page you are looking for does not exist 😥</p>
-        <button className="mx-auto flex items-center justify-center border-2 border-black bg-black px-6 py-2 text-white dark:border-white dark:bg-white dark:text-black lg:hover:border-2 lg:hover:border-black lg:hover:bg-white lg:hover:text-black lg:dark:hover:border-white lg:dark:hover:bg-black lg:dark:hover:text-white">
+        <button className="mx-auto flex items-center justify-center border-2 border-black bg-black px-6 py-2 text-white duration-300 dark:border-white dark:bg-white dark:text-black lg:hover:border-2 lg:hover:border-black lg:hover:bg-white lg:hover:text-black lg:dark:hover:border-white lg:dark:hover:bg-black lg:dark:hover:text-white">
           <Link to="/">Go back home</Link>
         </button>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="mx-8 -my-6 flex h-screen flex-col items-center justify-center space-y-8 px-6 text-center font-serif text-4xl dark:text-white md:mx-36 lg:mx-72">
-      {/* Find other way to dead-center div above */}
+    <div className="mx-8 my-12 flex h-screen flex-col items-center justify-start space-y-8 px-6 text-center font-serif text-4xl dark:text-white md:mx-36 lg:my-36 lg:mx-72">
       <p className="opacity-75">Hi, I'm</p>
-      <p className="text-7xl">Yin Chu</p>
+      <p className="text-6xl">Yin Chu</p>
       <p className="opacity-75">
         Web developer from Athens. I create stuff for the internet.
       </p>
-      <button className="flex items-center border-2 border-black bg-black px-6 py-2 text-white opacity-75 dark:border-white dark:bg-white dark:text-black lg:hover:border-2 lg:hover:border-black lg:hover:bg-white lg:hover:text-black lg:dark:hover:border-white lg:dark:hover:bg-black lg:dark:hover:text-white">
+      <button className="flex items-center border-2 border-black bg-black px-6 py-2 text-white opacity-75 duration-300 dark:border-white dark:bg-white dark:text-black lg:hover:border-2 lg:hover:border-black lg:hover:bg-white lg:hover:text-black lg:dark:hover:border-white lg:dark:hover:bg-black lg:dark:hover:text-white">
         <Link to="/about">About me</Link>
       </button>
     </div>
